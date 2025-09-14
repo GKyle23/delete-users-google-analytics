29e6848331df4f12577e7cd0c379516ab2c07a31
George Kyle <gskyle@gmail.com>
2025-09-15 11:33:23 +1200

    Create ua_delete_users.py

---
+++ legacy/ua_delete_users.py
@@ -0,0 +1,53 @@
+import csv
+import time
+from retrying import retry
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+import httplib2
+
+def get_service(api_name, api_version, scope, key_file_location, service_account_email):
+
+    #Parse command-line arguments.
+    credentials = ServiceAccountCredentials.from_p12_keyfile(
+        service_account_email, key_file_location, scopes=scope)
+
+    http = credentials.authorize(http=httplib2.Http())
+
+    #Build the service object.
+    service = build(api_name, api_version, http=http)
+
+    return service
+
+# open a list of user link ids
+with open('delete_ids.csv', 'rt') as csvfile:
+    reader = csv.reader(csvfile, dialect='excel')
+    reader_list = list(reader)
+    for row in reader_list:
+        print(row[0])
+
+
+@retry
+
+def delete_users(service):
+    for row in reader_list:
+        service.management().accountUserLinks().delete(
+        accountId ='XXXXXXXX',                          # reference your Google Analytics account here
+        linkId = row[0]
+        ).execute()
+        print(str(row[0]) + ' has been deleted')
+        time.sleep(30)
+
+
+def main():
+    # Define the auth scopes to request.
+    scope = ['https://www.googleapis.com/auth/analytics.manage.users']
+    service_account_email = 'your-management@projectname.iam.gserviceaccount.com'
+    key_file_location = 'client_secrets.p12'
+
+    # Authenticate and construct service.
+    service = get_service('analytics', 'v3', scope, key_file_location, service_account_email)
+    delete_users(service)
+
+
+if __name__ == '__main__':
+    main()
